888b4daa77bf2bfc3505c683e4ed4a89fc5ca57e
adarsh <thindpalak19@gmail.com>
2020-09-03 22:31:30 +0530

    cc square done

---
+++ week02/day04/squarecc.py
@@ -0,0 +1,10 @@
+print ("program print square", end=" ")
+a = 4
+square = a*a
+print("our square of 4 is", square)
+b = 2
+square = b*b
+print("our square of 2 is", square)
+c = 6
+square = c*c
+print("our square of 6 is", square)
